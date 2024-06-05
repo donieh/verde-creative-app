e401c9e2e90b3871caac0dffd4dff574c38c52d3
yopita <yopita@itbss.ac.id>
2024-06-05 09:55:48 +0700

    manajemen pelanggan clear

---
+++ app/Http/Controllers/ClientController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Client;
+use Illuminate\Http\Request;
+
+class ClientController extends Controller
+{
+    
+    public function index()
+    {
+        return view('client.index');
+    }
+
+    public function create()
+    {
+        return view('client.create');
+    }
+
+    public function store(Request $request)
+    {
+        $client = Client::create([
+            'code' => $request->code,
+            'name' => $request->name,
+            'contactPerson' => $request->contactPerson,
+            'phone' => $request->phone,
+            'address' => $request->address,
+        ]);
+
+        return redirect()->to('/client');
+    }
+
+    public function edit($clientId)
+    {
+        $client = Client::findOrFail($clientId);
+        return view('client.edit', [
+            'client' => $client,
+        ]);
+    }
+
+    public function update(Request $request,$clientId)
+    {
+        $client = Client::where('id',$clientId)->update([
+            'code' => $request->code,
+            'name' => $request->name,
+            'contactPerson' => $request->contactPerson,
+            'phone' => $request->phone,
+            'address' => $request->address,
+        ]);
+
+        return redirect()->to('/client');
+    }
+
+    public function destroy($clientId)
+    {
+        Client::where('id', $clientId)->delete();
+
+        return redirect()->to('/client');
+    }
+}
--- resources/views/user/edit.blade.php
+++ resources/views/user/edit.blade.php
@@ -3,10 +3,10 @@
 @section('panel_content')
     <div class="container-fluid">
         <!-- Page Heading -->
-        <h1 class="h3 mb-2 text-custom" style="color:black">Edit Data Pelanggan</h1>
+        <h1 class="h3 mb-2 text-custom" style="color:black">Edit Data User</h1>
         <div class="card shadow mb-4">
             <div class="card-header py-3">
-                <h6 class="m-0 font-weight-bold text-custom" style="color:black">Form Edit Pelanggan</h6>
+                <h6 class="m-0 font-weight-bold text-custom" style="color:black">Form Edit User</h6>
             </div>
             <form action="/user/{{$staff->id}}" method="POST">
                 @method('PUT')
