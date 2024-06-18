46e0b4e3ea0159861dd317f577eaa126cb922130
donieh <doni.purnama@itbss.ac.id>
2024-06-19 01:01:30 +0700

    edit table kecil

--- app/Http/Controllers/InvoiceController.php
+++ app/Http/Controllers/InvoiceController.php
@@ -7,7 +7,6 @@ use Illuminate\Http\Request;
 use App\Models\Client;
 use App\Models\Item;
 use App\Models\Package;
-use App\Models\InvoiceItem;
 
 class InvoiceController extends Controller
 {
@@ -29,7 +28,7 @@ class InvoiceController extends Controller
     {
         $invoiceDate = $request->invoiceDate;
         $dueDate = date('Y-m-d', strtotime($invoiceDate . ' +15 days'));
-    
+
         $invoice = Invoice::create([
             'invoiceDate' => $invoiceDate,
             'startDate' => $request->startDate,
@@ -40,39 +39,18 @@ class InvoiceController extends Controller
             'discount' => $request->discount,
             'downPayment' => $request->downPayment
         ]);
-    
-        $invoiceItems = json_decode($request->invoiceItems, true);
-    
-        foreach ($invoiceItems as $item) {
-            InvoiceItem::create([
-                'invoiceId' => $invoice->id,
-                'itemId' => $item['itemId'],
-                'packageId' => $item['packageId'],
-                'quantity' => $item['quantity']
-            ]);
-        }
-    
+
         return redirect()->to('/transaction');
     }
-    
-
-    // public function edit($invoiceId)
-    // {
-    //     $invoice = Invoice::findOrFail($invoiceId);
-    //     $clients = Client::all();
-    //     $items = Item::all();
-    //     $packages = Package::all();
-    //     return view('transaction.edit', compact('invoice', 'clients', 'items', 'packages'));
-    // }
 
     public function edit($invoiceId)
-{
-    $invoice = Invoice::with('items.item', 'items.package')->findOrFail($invoiceId);
-    $clients = Client::all();
-    $items = Item::all();
-    return view('transaction.edit', compact('invoice', 'clients', 'items'));
-}
-    
+    {
+        $invoice = Invoice::findOrFail($invoiceId);
+        $clients = Client::all();
+        $items = Item::all();
+        $packages = Package::all();
+        return view('transaction.edit', compact('invoice', 'clients', 'items', 'packages'));
+    }
 
     public function update(Request $request, $invoiceId)
     {
