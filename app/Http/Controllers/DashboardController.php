d0efd6d260f62e6e05be29f60f257ebb704d3fdc
yopita <yopita@itbss.ac.id>
2024-06-22 11:46:16 +0700

    m

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -22,7 +22,7 @@ class DashboardController extends Controller
         // Contoh data transaksi per bulan
         $monthlyInvoices = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120]; // Data dummy
 
-        $totalStaff = 100;
+        $totalStaff = '100';
 
         //print_r($totalStaff);   
         return view('dashboard.index', compact('totalStaff', 'totalClients', 'totalInvoices', 'monthlyNewStaff', 'monthlyInvoices'));
