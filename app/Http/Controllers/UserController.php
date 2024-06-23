fc78938e728590fc593528bed7f2463234dbfe37
donieh <doni.purnama@itbss.ac.id>
2024-06-24 00:20:16 +0700

    store pass lama user

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -38,17 +38,24 @@ class UserController extends Controller
         ]);
     }
 
-    public function update(Request $request,$staffId)
-    {
-        $staff = Staff::where('id',$staffId)->update([
-            'name' => $request->name,
-            'position' => $request->position,
-            'username' => $request->username,
-            'password' => md5($request->password),
-        ]);
+    public function update(Request $request, $staffId)
+{
+    $staff = Staff::findOrFail($staffId);
 
-        return redirect()->to('/user');
+    // Update data lainnya
+    $staff->name = $request->input('name');
+    $staff->position = $request->input('position');
+    $staff->username = $request->input('username');
+
+    // Periksa apakah password baru diisi atau tidak kosong
+    if ($request->filled('password')) {
+        $staff->password = md5($request->input('password'));
     }
+
+    $staff->save();
+
+    return redirect()->to('/user');
+}
 
     public function destroy($staffId)
     {
